9996dc17181aeb14c03e7bab7d241b7ca08e49da
npow <nissan.pow@gmail.com>
2014-07-23 11:52:49 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
-fred
-====
+#Fred
+Basically the same thing as https://github.com/reonomy/node-fred
+for browser usage (request Mikeal's request with jQuery)
 
-JS wrapper for the St. Louis Fed's FRED API
+![FRED Logo](http://research.stlouisfed.org/images/fred-logo-2x.png)
+
+### Usage
+```
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.js"></script>
+<script type="text/javascript" src="fred.js"></script>
+```
+
+### API Key
+Fred and the FRED API require and API key. You can [request an api key from the official Federal Reserve web site](http://api.stlouisfed.org/api_key.html).
+
+### Overview
+
+Create an instance of `fred` with your API key:
+```javascript
+var fred = Fred('GiVeMe1APIKeYPLeaSe');
+```
+
+Request a resource:
+```javascript
+fred.series.observations('GDP', function(err, grossNationalProduct) {
+  if (!err) console.log(grossNationalProduct.observations)
+});
+```
+
+
+### More information
+
+The [documentation for the FRED API](http://api.stlouisfed.org/docs/fred/#General_Documentation) is available on the Federal Reserve's web site.
---
+++ fred.js
@@ -0,0 +1,118 @@
+var request = function(args, done) {
+  $.ajax({
+    type: 'GET',
+    url: 'http://jsonp.jit.su/?callback=?&url=' + encodeURIComponent(args.uri + '?' + $.param(args.qs)),
+    dataType: 'jsonp',
+    success: function (data) {
+      done(null, { statusCode: 200 }, data);
+    }
+  });
+};
+
+var DEFAULT_FRED = {
+  PROTOCOL: 'http://',
+  HOST: 'api.stlouisfed.org',
+  PATH: '/fred'
+}
+
+var Fred = function(apiKey) {
+  if (!(this instanceof Fred)) {
+    return new Fred(apiKey)
+  }
+
+  this._api = {
+    protocol: DEFAULT_FRED.PROTOCOL,
+    host: DEFAULT_FRED.HOST,
+    path: DEFAULT_FRED.PATH,
+    key: apiKey
+  }
+
+  this.category.children = Fred.prototype.category.children.bind(this)
+  this.category.related = Fred.prototype.category.children.bind(this)
+  this.series.observations = Fred.prototype.series.observations.bind(this)
+}
+
+Fred.prototype.category = function(categoryId, done) {
+  request({
+    uri: 'http://' + this._api.host + this._api.path + '/category',
+    qs: {
+      file_type: 'json',
+      api_key: this._api.key,
+      category_id: categoryId
+    },
+    json: true
+  }, function(err, res, body) {
+    if (err) return done(err)
+    if (res.statusCode != 200) return done(new Error(body.error_message))
+
+    done(null, body.categories[0])
+  })
+}
+
+Fred.prototype.category.children = function(categoryId, done) {
+  request({
+    uri: this._api.protocol + this._api.host + this._api.path + '/category/children',
+    qs: {
+      file_type: 'json',
+      api_key: this._api.key,
+      category_id: categoryId
+    },
+    json: true
+  }, function(err, res, body) {
+    if (err) return done(err)
+    if (res.statusCode != 200) return done(new Error(body.error_message))
+
+    done(null, body.categories)
+  })
+}
+
+Fred.prototype.category.related = function(categoryId, done) {
+  request({
+    uri: this._api.protocol + this._api.host + this._api.path + '/category/related',
+    qs: {
+      file_type: 'json',
+      api_key: this._api.key,
+      category_id: categoryId
+    },
+    json: true
+  }, function(err, res, body) {
+    if (err) return done(err)
+    if (res.statusCode != 200) return done(new Error(body.error_message))
+
+    done(null, body.categories)
+  })
+}
+
+Fred.prototype.series = function(seriesId, done) {
+  request({
+    uri: this._api.protocol + this._api.host + this._api.path + '/series',
+    qs: {
+      file_type: 'json',
+      api_key: this._api.key,
+      series_id: seriesId
+    },
+    json: true
+  }, function(err, res, body) {
+    if (err) return done(err)
+    if (res.statusCode != 200) return done(new Error(body.error_message))
+
+    done(null, body)
+  })
+}
+
+Fred.prototype.series.observations = function(seriesId, done) {
+  request({
+    uri: this._api.protocol + this._api.host + this._api.path + '/series/observations',
+    qs: {
+      file_type: 'json',
+      api_key: this._api.key,
+      series_id: seriesId
+    },
+    json: true
+  }, function(err, res, body) {
+    if (err) return done(err)
+    if (res.statusCode != 200) return done(new Error(body.error_message))
+
+    done(null, body)
+  })
+}
